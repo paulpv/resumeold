ea7c86c4f1e164e235cfa7d972f71bfd6620246f
Paul Peavyhouse <pv@swooby.com>
2015-01-23 17:25:53 -0800

    Getting FontSize to work in iOS

--- Shared/Views/AboutView.cs
+++ Shared/Views/AboutView.cs
@@ -7,33 +7,36 @@ namespace Resume.Shared
 	{
 		public AboutView ()
 		{
-			var stack = new StackLayout {
+			var stack = new StackLayout
+            {
 				Orientation = StackOrientation.Vertical,
-				Spacing = 10
+				Spacing = 10,
 			};
 
-			var image = new Image ();
-			image.Source = ImageSource.FromFile ("pv_batman.jpg");
+			var image = new Image();
+			image.Source = ImageSource.FromFile("pv_batman.jpg");
 			image.Aspect = Aspect.AspectFill;
 
-			stack.Children.Add (image);
+			stack.Children.Add(image);
 
-			var stack2 = new StackLayout {
+			var stack2 = new StackLayout
+            {
 				Orientation = StackOrientation.Vertical,
 				VerticalOptions = LayoutOptions.FillAndExpand,
 				Spacing = 10,
-				Padding = 10
+				Padding = 10,
 			};
 
-			var about = new Label {
-                FontSize = Font.SystemFontOfSize(NamedSize.Medium).FontSize,
+            var about = new Label
+            {
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
 				Text = "My name is Paul Peavyhouse...",
-				LineBreakMode = LineBreakMode.WordWrap
+				LineBreakMode = LineBreakMode.WordWrap,
 			};
 
 			stack2.Children.Add(about);
 
-			stack.Children.Add(new ScrollView{VerticalOptions = LayoutOptions.FillAndExpand, Content = stack2 });
+			stack.Children.Add(new ScrollView { VerticalOptions = LayoutOptions.FillAndExpand, Content = stack2 });
 
 			Content = stack;
 		}
